987f247193667e884dcc595bde40ef1ba93c29e6
liuenda <liuenda123@hotmail.com>
2016-07-18 10:37:30 +0900

    initial state

---
+++ find_vecs_en.py
@@ -0,0 +1,158 @@
+#This script could retrieve the vecoter representatiton for a give text word(pharse) lists
+#basing on the word2vec model trained
+#Edit 16/3/15
+#Edit 16/3/17
+#Normalizing the vector from a trained model so that all vector will be in length 
+
+# coding:utf-8
+import sys  
+import time
+start_time = time.time()
+import gensim, logging
+import numpy as np
+from gensim.models import *
+from nltk.stem.wordnet import WordNetLemmatizer
+import nltk
+from numpy import linalg as LA
+import pandas as pd
+import csv
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+wnl = WordNetLemmatizer()
+
+def getBase(str,lm):
+	str=lm.lemmatize(str, 'v')
+	str=lm.lemmatize(str, 'n')
+	return str;
+
+def nans(shape, dtype=float):
+    a = np.empty(shape, dtype)
+    a.fill(np.nan)
+    return a;
+
+def vecNorm(vector):
+	vectorNorm=vector/LA.norm(vector)
+	return vectorNorm
+
+logout=open('./output/Log/log_en.txt','w')
+model_name='../Modeling/model-en/W2Vmodle.bin'
+input_filename='./input/keyword_103_en.txt'
+output_filename2=open('./output/Log/bad_list_en.txt','w')
+output_filename='output/vecs_en.csv'
+#fout=open('./input/goodlist_e.txt','w')
+
+model = Word2Vec.load(model_name)
+good_list=[]
+counter_NaN=0
+count_phrase=0
+count_word=0
+wnl = WordNetLemmatizer()
+dim=200
+
+with open(input_filename) as vocab_file:
+	for index,vocab in enumerate(vocab_file):
+		#rawVoc=vocab #keep the original un-processed word or phrase!
+		#vocab=vocab.rstrip() # remove all '\n' and '\r'
+		#vocab=vocab.lower()
+		
+		#Remove rawVoc from the raw Data is very needed!
+		rawVoc=vocab.rstrip() # remove all '\n' and '\r'
+		vocab=rawVoc.lower()
+		
+		info=""
+
+		# If the line is a SINGLE word
+		if " " not in vocab:
+			count_word+=1
+			print "word",vocab
+			baseform=getBase(vocab,wnl)
+			try:
+				vecW=model[baseform] #!!!Maybe the word is not existed
+			except Exception,e:
+				counter_NaN+=1 #increase 1 to NaN counter
+				info+=repr(e)+"\n" #create log information
+				logout.write(info) #write log information to log file				
+				#new 3.15: generate a useless list for deleting in the next stage
+				output_filename2.write(rawVoc) # no \n is needed since the 
+			else:
+				vecW=vecNorm(vecW) #Normalized the raw vector
+				print "the new length of the vector is:"
+				print LA.norm(vecW)
+				info+=baseform+": OK!\n" #create log information
+				logout.write(info) #write log information to log file
+				# fout.write(rawVoc) #add in 16/3/17
+				good_list.append(rawVoc)
+				#append the new vector to the matrix
+				#if the vector is the first element in the matrix: 'good_vecs', reshape it
+				if index==0: 
+					good_vecs=vecW.reshape(1,dim)
+				else:
+					good_vecs=np.concatenate((good_vecs,vecW.reshape(1,dim)),axis=0)
+
+		# If the line is a PHRASE --------
+		else:
+			count_phrase+=1
+			tokens = nltk.word_tokenize(vocab)
+			tagged = nltk.pos_tag(tokens)
+			vecP=np.zeros(dim)
+			#print tagged
+			modified=[]
+			for vocabP in tagged:
+				flag_nan=False
+				if vocabP[1]!="IN": # ignore the IN components
+					modified.append(vocabP[0])
+					baseformP=getBase(vocabP[0],wnl)
+					try:
+						vec1word=model[baseformP] #!!!Maybe the word is not existed
+					except Exception,e:
+						#print repr(e)
+						info+=repr(e)+" " #create log information
+						counter_NaN+=1 #increase the nan counter
+						flag_nan=True
+						break #if exception occrus, stop the for loop
+						#vecP+=nans([1,dim]) 
+					else:
+						vecP+=vec1word #add vectors
+						info+=baseformP+": OK!; " 
+						#print vec
+
+			logout.write(info+"\n") #write log information to log file
+			print "phrase:",modified #print the good phrase
+
+			# if the phrase is all good, nomalize it and then save it to the goodlist file (fout) and save the array to good_vecs
+			if flag_nan==False:
+				#Normalize the added raw phrase vector to unit length
+				vecP=vecNorm(vecP)
+				print "the new length of the vector is:"
+				print LA.norm(vecP)
+
+				#fout.write(" ".join(modified)+"\n") #here is a problem which will produce one more line here
+				# fout.write(rawVoc)
+				good_list.append(rawVoc)
+				if index==0:
+					good_vecs=vecP.reshape(1,dim)
+				else:
+					good_vecs=np.concatenate((good_vecs,vecP.reshape(1,dim)),axis=0)
+			else:
+				#new 3.15: generate a useless list for deleting in the next stage
+				output_filename2.write(rawVoc)
+
+	print "NaN words:",counter_NaN
+	print "Pharses Number:",count_phrase
+	print "all lines:",index+1
+	print "all word:",count_word
+
+
+	# Create a pandas Series
+	df = pd.DataFrame(good_vecs,index=good_list)
+	df.to_csv(output_filename,index=True,quoting=csv.QUOTE_NONNUMERIC,encoding='utf-8')
+
+	#save the array to file
+	# np.save('./output/vectors_e',good_vecs)
+	# np.savetxt('./output/vectors_e.txt',good_vecs)
+	#When run under the Ipython, close() is necessary preventing from 0kb issues
+	# fout.close()
+	output_filename2.close()
+	logout.close()
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ statistic.py
@@ -0,0 +1,118 @@
+#This script could do statistics for English and Japanese 
+
+# coding:utf-8
+from __future__ import division
+import csv
+import pandas as pd
+import numpy as np
+from itertools import combinations
+#In order to get float result when dividing
+
+k=10
+
+def merge(k):
+	input_filename1='output/jp'+str(k)+'_good.csv'
+	input_filename2="output/good_dic.csv"
+	input_filename3='output/en'+str(k)+'_good.csv'
+
+	output_filename='output/merge'+str(k)+'.csv'
+
+	df_jp=pd.read_csv(input_filename1,header=0)
+	df_dic=pd.read_csv(input_filename2,header=0)
+	df_en=pd.read_csv(input_filename3,header=0)
+
+	df=df_dic
+	#df['en10']=df_en['k10']
+	en10=[]
+	for x in df['en']:
+		#print x
+		# Becareful for the poetential Unicode problem in this comparison
+		result=df_en.loc[df_en['en'] == x]
+		print result
+		en10.append(result['k10'].iloc[0])
+	#print jp10
+	df['en10']=pd.Series(en10)
+
+	jp10=[]
+	for x in df['jp']:
+		#print x
+		# Becareful for the poetential Unicode problem in this comparison
+		result=df_jp.loc[df_jp['jp'] == x]
+		#print result
+		jp10.append(result['k10'].iloc[0])
+	#print jp10
+	df['jp10']=pd.Series(jp10)
+
+	df.to_csv(output_filename,index=False,quoting=csv.QUOTE_NONNUMERIC,encoding='utf-8')
+
+#Get a sum of NC(number of common words) for one combination
+#NOTE, DO NOT PASTE the whole function to ipython at once!
+#combin is a list, NC is a tuple
+def getNCSum(combin,NC):
+	sum=0
+	for x in combin:
+		sum+=NC[x]
+	#print "NC sum for combination:",combin," is ",sum
+	return sum
+
+def getNSum(combin,N):
+	sum=0
+	for x in combin:
+		sum+=N[x]
+	#print "N sum for combination:",combin," is ",sum
+	return sum
+
+
+merge(k)
+
+input_filename='output/merge'+str(k)+'.csv'
+df=pd.read_csv(input_filename)
+counter=[]
+for value in range(1,k+1):
+	sub_df = df.ix[(df['en10']==value)]
+	ser=sub_df['jp10']
+	count=ser.value_counts()
+	#print "for the gourp",value,"they have:"
+	#print count
+	counter.append(count)
+#print counter
+
+#The totla number of word for each JP group
+N=df.jp10.value_counts()
+
+max_sum=0
+for N_en in range(1,k+1):
+	NC=counter[N_en-1]
+	N_en_counter=df.en10.value_counts()
+	N_en_count=N_en_counter[N_en]
+	print "NC in group ",N_en,"is \n",NC
+	NC_name_list=NC.index.values
+	
+	#Get the combination list
+	combin_list=[]
+	for i in range(1,len(NC)+1):
+		combin_list+=list(combinations(NC_name_list, i))
+	print "size of the combination list is in group ",N_en," is ",len(combin_list)
+
+	#map() is not useful in this case although it is meaningful
+	NC_sum_list=[getNCSum(x,NC) for x in combin_list]
+	
+	#PLEASE NOTE: DO NOT forget to add the number of the EN cluster (only 1)
+	N_sum_list=[getNSum(x,N)+N_en_count for x in combin_list]
+
+	sim_list=map(lambda x,y:2*x/y,NC_sum_list,N_sum_list)
+	#sim_list=2*NC_sum_list/N_sum_list
+	#sim_list=[2*a/b for a,b in zip(NC_sum_list,N_sum_list)]
+	#print sim_list
+
+	#Find the index of maximu similarity
+	sim_bestIndex=sim_list.index(max(sim_list))
+	print "the best combination similiar to EN group ",N_en,"is ",combin_list[sim_bestIndex]
+	print "with the similarity of ",max(sim_list)
+	print "-----------------"
+ 	max_sum+=max(sim_list)
+	#Create a DataFrame for print
+	#df_result=pd.DataFrame()
+print "Average similairty is: ",max_sum/k
+
+
